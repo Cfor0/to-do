327ef0be09bec7bb1ad881429ebca7ebc6d9a103
cfor0 <cfor0@outlook.com>
2021-01-20 10:04:41 -0500

    added git repo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import { Component } from 'react';
+import List from './components/List';
+
+class App extends Component {
+  // tracking userInput: using form or an input field
+  // onChange is probably needed
+  // onSubmit is needed
+  // Probably need a prevent default
+  // state to keep the information the same and only change when I need it to change
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      noteText: '',
+      notes: []
+    }
+  }
+
+  onChange = (event) => {
+    this.setState({
+      noteText: event.target.value
+    })
+  }
+
+  onSubmit = (event) => {
+    event.preventDefault()
+    this.setState({
+      noteText: '',
+      notes: [...this.state.notes, this.state.noteText]
+    })
+    console.log(this.state.notes)
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <form onSubmit={this.onSubmit}>
+          <input value={this.state.noteText} onChange={this.onChange} placeholder='Type here' />
+
+        </form>
+        <List notes = {this.state.notes} />
+      </div>
+    );
+  }
 }
 
 export default App;
